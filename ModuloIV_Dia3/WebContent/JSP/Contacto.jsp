25a10e6c026dab3bfea81354454c0fa9b48eede1
Cathy-B <56276886+Cathy-B@users.noreply.github.com>
2020-12-29 13:52:58 -0300

    agregar ejercicio 3 mod 4
    agregó ejericio grupal 3 modulo IV a repositorio

---
+++ ModuloIV_Dia3/WebContent/JSP/Contacto.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link rel="stylesheet" href="CSS/estilo.css">
+<title>Contacto</title>
+</head>
+<body>
+<nav>
+            <a href="/html/">Listar usuarios</a> |
+            <a href="/css/">Crear usuario</a> |
+            <a href="/js/">Editar cliente</a> |
+            <a href="/python/">Editar adminisrtativo</a>|
+            <a href="/python/">Editar profesional</a>
+           
+          </nav>
+        <h2>Menu de navegación</h2>
+        <ol type="A"> 
+            <li> <a href="C:\Users\Admini\Documents\daniela\05 - estudios\JAVA\MODULO 3\CLASE 2\Proyecto grupal 2\index.html" target="_blank">Pagina de inicio</a></li>
+        <li> <a href="C:\Users\Admini\Documents\daniela\05 - estudios\JAVA\MODULO 3\CLASE 2\Proyecto grupal 2\contacto.html" target="_blank">Contacto</a></li>
+        </ol>
+        <h1>Favor rellene la información solicitada para contactarse</h1>
+        <ol type="A"> 
+            <li > Ingrese nombre: <input></li>
+            <li> Ingrese correo: <input></li>
+            <li> Ingrese telefono: <input></li>
+            <li> Ingrese mensaje: <input></li>
+        </ol>
+</body>
+</html>
---
+++ ModuloIV_Dia3/src/cl/awakelab/grupal3/ServletCrearCapacitacion.java
@@ -0,0 +1,41 @@
+package cl.awakelab.grupal3;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletCrearCapacitacion
+ */
+@WebServlet("/ServletCrearCapacitacion")
+public class ServletCrearCapacitacion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletCrearCapacitacion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		getServletContext().getRequestDispatcher("/JSP/CrearCapacitacion.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
